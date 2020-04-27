03cc1681e2363db9cdb6a1c39b98ef24debb5bf0
Benjamin Steier <bensteier713@gmail.com>
2020-04-27 15:31:45 -0500

    Removed initial configuration packages and included new package for the main window

---
+++ src/com/windows/main/design/ConsolidateGameWindow.java
@@ -0,0 +1,9 @@
+package com.windows.main.design;
+
+public class ConsolidateGameWindow {
+
+    /*
+    Simple class that will initialize the game window and import all other packages.
+     */
+
+}
